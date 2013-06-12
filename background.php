d3ac3471bf3afbfa0b4d24252653d59775fecc82
Anthony Armstrong <anthony.armstrong@propcom.co.uk>
2013-06-12 17:38:20 +0200

    fixed security vulnerability

--- background.php
+++ background.php
@@ -1,64 +1,81 @@
 <?php
 
-Class Controller_Background extends \Controller_Rest {
+class Controller_Background extends Controller_Rest {
 
 	public function before() {
-
-		// Throw the exception when invalid request (404 error).
+	
+            // Throw the exception when invalid request (404 error).
 	    if (!\Input::is_ajax()) {
 	        throw new \Request404Exception();
 	    }
-
-		// Get page from request
-		$this->folder = Input::get('p', 'index');
-
-		// Build directory path
-		$this->path    = DOCROOT . 'assets/templates/funkin/img/backgrounds/' . $this->folder;
-		$this->js_path = 'assets/templates/funkin/img/backgrounds/' . $this->folder;
-
-		$this->bg_images = array();
-
-		parent::before();
-
+	
+            // safely build directory path
+	    $this->directory = DOCROOT . 'assets/img/transitions/';
+	    $this->folder = realpath($this->directory . Input::get('p'));
+	
+	    // set default
+	    if ($this->folder == false) {
+	    	$this->folder = 'index';
+	    } 
+	
+	    $this->path    = $this->directory . basename($this->folder);
+	    $this->js_path = 'assets/img/transitions/' . basename($this->folder);
+	
+	    $this->bg_images = array();
+	
+	    parent::before();
+	
 	}
 
 
-	public function get_images() {
+        public function get_images() {
 
-		try { 
+        	try {
 
-			// Scan directory (jpg and png only)
-			$images = File::read_dir($this->path, 0, array(
-				'\.png$'  => 'file',
-				'\.jpg$'  => 'file',
-				)
-			);
+                	// Scan directory (jpg and png only)
+                        $images = File::read_dir($this->path, 0, array(
+                                '\.png$'  => 'file',
+                                '\.jpg$'  => 'file',
+                                )
+                        );
 
-			foreach ($images as $image) {
+                        foreach ($images as $image) {
+	                        // Build a full file path
+	                        $file_path = $this->js_path . '/' .$image;
+                                $this->bg_images[] = $file_path;
+                    	}
+	
+	                $this->response(array(
+                        	'result' => 'success',
+                        	'images' => $this->bg_images 
+	                ));
+	
+	        }
 
-	          	// Build a full file path
-	          	$file_path = $this->js_path . '/' .$image;
-	          	array_push($this->bg_images, $file_path);
-		        
-		    }
+                /* 
+                Invalid path exception, wont throw unless someone is trying to be cheeky
+               as we supply a default for input and are also using basename and realpath
+                */
+                catch(InvalidPathException $e) {
 
-		    $this->response(array(
-		    	'result' => 'success', 
-		    	'images' => $this->bg_images
-		    ));
+                        $this->response(array(
+                                'result' => 'error',
+                                'message' => 'I am an angry exception! Grrrr!'
+                        ));
+                                
+                }
 
-		} 
+                // all other exceptions...
+                catch(Exception $e) {
 
-		catch(Exception $e) {
+                        $this->response(array(
+                                'result' => 'error',
+                                'message' => $e->getMessage()
+                        ));
 
-			$this->response(array(
-				'result' => 'error', 
-				'message' => $e->getMessage()
-			));
+                }
 
-		} 
+        }
 
-
-	}
 
 }
